2dcb29a2965407cb2b4db1471325d3cb661e2dfc
Elite <petpanda0057@gmail.com>
2023-05-26 18:56:28 +0300

    feat: update title

--- resources/views/alergy/create.blade.php
+++ resources/views/alergy/create.blade.php
@@ -4,7 +4,7 @@
 <div class="row mb-5">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Add New Post</h2>
+            <h2>Add New Alergy</h2>
         </div>
         <div class="pull-right">
             <a class="btn btn-success" href="/"> Back</a>
